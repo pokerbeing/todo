819d0c232c4d8278b91f000d854ea330024f9f1b
PokerBeing <pokerbeing@yahoo.com>
2022-08-23 19:21:52 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>The ToDo App</h1>
+      <div class="flow-right controls">
+        <span>Item count: <span id="item-count">0</span></span>
+        <span>Uncompleted count: <span id="unchecked-count">0</span></span>
+      </div>
+      <button id="myBtn" class="button center">New TODO</button>
     </div>
-  );
+  )
 }
